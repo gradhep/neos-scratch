77852e9d317823326fc32f9fbc7a9a5ce5aa1735
Nathan Simpson <email.nsimpson@gmail.com>
2021-06-07 19:35:42 +0200

    update for reflect infinities

--- examples/3-blobs/maker.py
+++ examples/3-blobs/maker.py
@@ -9,6 +9,7 @@ def yield_maker(
     bins=jnp.array([-jnp.inf, *jnp.linspace(-100, 100, 3), jnp.inf]),
     SCALE=1,
     syst=True,
+    reflect_infinities=True,
 ):
     def model(angle, anchr=jnp.array([0.0, 0.0])):
 
@@ -17,32 +18,36 @@ def yield_maker(
         # direc = jnp.array([jnp.cos(angle),jnp.sin(angle)])
         normal = jnp.array([jnp.cos(angle + jnp.pi / 2), jnp.sin(angle + jnp.pi / 2)])
 
-        hb1 = hist_kde(jnp.matmul((b1 - anchr), normal), bins=bins, bandwidth=bandwidth)
-        hb2 = hist_kde(jnp.matmul((b2 - anchr), normal), bins=bins, bandwidth=bandwidth)
-        hb3 = hist_kde(jnp.matmul((b3 - anchr), normal), bins=bins, bandwidth=bandwidth)
+        hb1 = hist_kde(
+            jnp.matmul((b1 - anchr), normal),
+            bins=bins,
+            bandwidth=bandwidth,
+            reflect_infinities=reflect_infinities,
+        )
+        hb2 = hist_kde(
+            jnp.matmul((b2 - anchr), normal),
+            bins=bins,
+            bandwidth=bandwidth,
+            reflect_infinities=reflect_infinities,
+        )
+        hb3 = hist_kde(
+            jnp.matmul((b3 - anchr), normal),
+            bins=bins,
+            bandwidth=bandwidth,
+            reflect_infinities=reflect_infinities,
+        )
 
-        hs1 = hist_kde(jnp.matmul((s1 - anchr), normal), bins=bins, bandwidth=bandwidth)
+        hs1 = hist_kde(
+            jnp.matmul((s1 - anchr), normal),
+            bins=bins,
+            bandwidth=bandwidth,
+            reflect_infinities=reflect_infinities,
+        )
 
-        nb1 = (
-            hb1[1:-1]
-            + jnp.array([hb1[0]] + [0] * (len(hb1) - 3))
-            + jnp.array([0] * (len(hb1) - 3) + [hb1[-1]])
-        ) / SCALE
-        nb2 = (
-            hb2[1:-1]
-            + jnp.array([hb2[0]] + [0] * (len(hb2) - 3))
-            + jnp.array([0] * (len(hb1) - 3) + [hb2[-1]])
-        ) / SCALE
-        nb3 = (
-            hb3[1:-1]
-            + jnp.array([hb3[0]] + [0] * (len(hb3) - 3))
-            + jnp.array([0] * (len(hb1) - 3) + [hb3[-1]])
-        ) / SCALE
-        ns1 = (
-            hs1[1:-1]
-            + jnp.array([hs1[0]] + [0] * (len(hs1) - 3))
-            + jnp.array([0] * (len(hb1) - 3) + [hs1[-1]])
-        ) / SCALE
+        nb1 = (hb1) / SCALE
+        nb2 = (hb2) / SCALE
+        nb3 = (hb3) / SCALE
+        ns1 = (hs1) / SCALE
 
         if not syst:
             nb1, nb2, nb3 = nb2, nb2, nb2
